9b11e8313ecb41328719ff89924f90b768dc5d20
f-adi <fadi.baaklini@gmail.com>
2022-10-01 12:57:56 -0400

    test

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const flights =
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
+  names: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],

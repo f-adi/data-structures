f16ab603832ac7e98248263ccd90381ca0c92d13
f-adi <fadi.baaklini@gmail.com>
2022-12-12 11:13:48 -0500

    Coding Challeng #2

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ const restaurant = {
   },
 };
 
+/*
 // Property Names
 const properties = Object.keys(openingHours);
 console.log(properties);
@@ -408,3 +409,62 @@ console.log(...game.scored);
 team1 < team2 && console.log('Team 1 is more likely to win');
 team2 > team1 && console.log('Team 2 is more likely to win');
 */
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Coding Challenge #2
+//.1
+
+for (const [i, scorers] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${scorers}`);
+}
+
+//.2
+const gameOdds = Object.values(game.odds);
+console.log(gameOdds);
+
+let total = 0;
+for (let i = 0; i < gameOdds.length; i++) {
+  total += gameOdds[i];
+}
+const avgOdd = total / gameOdds.length;
+console.log(avgOdd);
